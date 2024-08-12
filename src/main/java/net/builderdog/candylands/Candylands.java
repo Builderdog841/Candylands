ce9d3a0ac8e4d32025837ed2f7d31b1bd6e7b0f3
Builderdog841 <timon.kempter@gmail.com>
2024-08-12 19:09:39 +0200

    Basic Workspace Set-up

---
+++ src/main/java/net/builderdog/candylands/Candylands.java
@@ -0,0 +1,50 @@
+package net.builderdog.candylands;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(Candylands.MODID)
+public class Candylands {
+    public static final String MODID = "candylands";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Candylands(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+
+        // BLOCKS.register(modEventBus);
+        // ITEMS.register(modEventBus);
+        // CREATIVE_MODE_TABS.register(modEventBus);
+
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
