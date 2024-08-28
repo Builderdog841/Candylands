5de77d30e167d9355ff35e32703991737577dcc0
Builderdog841 <timon.kempter@gmail.com>
2024-08-28 18:26:00 +0200

    Some basic Block Registration

--- src/main/java/net/builderdog/candylands/block/CandylandsBlockBuilders.java
+++ src/main/java/net/builderdog/candylands/block/CandylandsBlockBuilders.java
@@ -1,4 +1,57 @@
 package net.builderdog.candylands.block;
 
+import net.builderdog.candylands.block.natural.JellyBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
+
 public class CandylandsBlockBuilders {
+
+    public static Block cream(MapColor mapColor) {
+        return new Block(
+                BlockBehaviour.Properties.of()
+                        .mapColor(mapColor)
+                        .strength(0.5F)
+                        .sound(SoundType.SNOW)
+        );
+    }
+
+    public static JellyBlock jelly(MapColor mapColor) {
+        return new JellyBlock(
+                BlockBehaviour.Properties.of()
+                        .mapColor(mapColor)
+                        .strength(0.4F)
+                        .sound(SoundType.HONEY_BLOCK)
+        );
+    }
+
+    public static RotatedPillarBlock candyCane(MapColor topMapColor, MapColor sideMapColor) {
+        return new RotatedPillarBlock(
+                BlockBehaviour.Properties.of()
+                        .mapColor(block -> block.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? topMapColor : sideMapColor)
+                        .instrument(NoteBlockInstrument.BASS)
+                        .strength(2.0F)
+                        .sound(SoundType.WOOD)
+                        .ignitedByLava()
+        );
+    }
+
+    public static boolean never(BlockState state, BlockGetter getter, BlockPos pos) {
+        return false;
+    }
+
+    public static boolean always(BlockState state, BlockGetter getter, BlockPos pos) {
+        return true;
+    }
+
+    public static <A> boolean never(BlockState state, BlockGetter getter, BlockPos pos, A block) {
+        return false;
+    }
 }
--- src/main/java/net/builderdog/candylands/block/CandylandsBlocks.java
+++ src/main/java/net/builderdog/candylands/block/CandylandsBlocks.java
@@ -5,6 +5,7 @@ import net.builderdog.candylands.item.CandylandsItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -12,8 +13,17 @@ import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class CandylandsBlocks {
+public class CandylandsBlocks extends CandylandsBlockBuilders {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Candylands.MODID);
+
+    public static final DeferredBlock<Block> GLAZE = register("glaze", () -> cream(MapColor.WOOL));
+    public static final DeferredBlock<Block> FROZEN_YOGHURT = register("frozen_yoghurt", () -> cream(MapColor.SNOW));
+    public static final DeferredBlock<Block> STRAWBERRY_CREAM = register("strawberry_cream", () -> cream(MapColor.COLOR_PINK));
+    public static final DeferredBlock<Block> VANILLA_CREAM = register("vanilla_cream", () -> cream(MapColor.SAND));
+    public static final DeferredBlock<Block> CHOCOLATE_CREAM = register("chocolate_cream", () -> cream(MapColor.COLOR_BROWN));
+
+    public static final DeferredBlock<Block> JELLY = register("jelly", () -> jelly(MapColor.TERRACOTTA_PINK));
+    public static final DeferredBlock<Block> DARK_JELLY = register("dark_jelly", () -> jelly(MapColor.TERRACOTTA_RED));
 
     private static <T extends Block> DeferredBlock<T> baseRegister(String name, Supplier<? extends T> block, Function<DeferredBlock<T>, Supplier<? extends Item>> item) {
         DeferredBlock<T> register = BLOCKS.register(name, block);
